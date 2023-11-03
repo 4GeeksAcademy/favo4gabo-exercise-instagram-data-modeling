cfc8968c56f01a51ebee2c0d0b0b0fae50b1095e
Favo4gabo <137852920+Favo4gabo@users.noreply.github.com>
2023-11-03 21:02:20 +0000

    listo

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Text
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
@@ -27,6 +27,42 @@ class Address(Base):
 
     def to_dict(self):
         return {}
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    user_name = Column(String(100), nullable=False, unique=True)
+    first_name = Column(String(100), nullable=False, unique=True)
+    last_name = Column(String(100), nullable=False, unique=True)
+    email = Column(String(200), nullable=False, unique=True)
+    follower = relationship('Follower', backref = 'user')
+    post = relationship('Post', backref = 'user')
+    comment = relationship('Comment', backref = 'user')
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('user.id'))
+    user_to_id = Column(Integer, ForeignKey('user.id'))
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key = True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    comment = relationship('Comment', backref = 'post')
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key = True)
+    coment_text = Column(Text())
+    author_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key = True)
+    url = Column(String(200))
+    post_id = Column(Integer, ForeignKey('post.id'))
 
 ## Draw from SQLAlchemy base
 try:
